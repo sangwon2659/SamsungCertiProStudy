734b663b8c55e0cd6a7143d754d7c92c5e1e9231
sangwon2659 <60720982+sangwon2659@users.noreply.github.com>
2025-03-23 15:29:15 +0900

    Update Search.cpp

--- Algorithms/Search.cpp
+++ Algorithms/Search.cpp
@@ -1,9 +1,72 @@
 #include <iostream>
 using namespace std;
 
+////////// Binary Search Start //////////
+
+#define MAX_M 100
+ 
+int T;    // # of test case
+int M;    // # of element in array
+int N;    // # of numbers to search
+int arr[MAX_M];
+ 
+void binarySearch(int* arr, int low, int high, int target)
+{
+    int mid;
+    if (low > high) 
+    {
+        printf("-1 ");
+        return;
+    }
+ 
+    mid = (low + high) / 2;
+ 
+    if (target < arr[mid])
+    {
+        binarySearch(arr, low, mid - 1, target);
+    }
+    else if (arr[mid] < target)
+    {
+        binarySearch(arr, mid + 1, high, target);
+    }
+    else
+    {
+        printf("%d ", mid);
+        return;
+    }
+}
+
+////////// Binary Search End //////////
+
+////////// DFS Search Start //////////
+
+////////// DFS Search End //////////
+
+////////// BFS Search Start //////////
+
+////////// BFS Search End //////////
+
+////////// Parametric Search Start //////////
+
+////////// Parametric Search End //////////
+
 int main()
 {
-  
+    ////////// Binary Search Start //////////
+
+    ////////// Binary Search End //////////
+
+    ////////// DFS Search Start //////////
+
+    ////////// DFS Search End //////////
+
+    ////////// BFS Search Start //////////
+
+    ////////// BFS Search End //////////
+
+    ////////// Parametric Search Start //////////
+
+    ////////// Parametric Search End //////////
 }
 
 /*
